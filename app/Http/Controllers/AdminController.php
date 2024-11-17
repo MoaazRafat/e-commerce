afc0bffec4f812c719389a78a97f5c6175a9fefe
MoaazRafat <mo3az.rafat@gmail.com>
2024-11-17 10:15:34 +0200

    E-Commerce

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Brand;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Laravel\Facades\Image;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view('admin.index');
+    }
+
+    public function brands()
+    {
+        $brands = Brand::orderBy('id', 'DESC')->paginate(10);
+        return view('admin.brands', compact('brands'));
+    }
+
+    public function add_brand()
+    {
+        return view('admin.brand-add');
+    }
+
+    public function store_brand(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:brands,slug',
+            'image' => 'mimes:jpg,png,jpeg|max:2048',
+        ]);
+
+        $brand = new Brand();
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->slug);
+        $image = $request->file('image');
+        $file_extension = $request->file('image')->extension();
+        $file_name = Carbon::now()->timestamp . '.' . $file_extension;
+        $this->GenerateBrandThumbnailsImage($image, $file_name);
+        $brand->image = $file_name;
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status', 'Brand Has Been Added Successfully!');
+    }
+
+    public function edit_brand($id)
+    {
+        $brand = Brand::find($id);
+    }
+
+    public function GenerateBrandThumbnailsImage($image, $image_name)
+    {
+        $destination_path = public_path('uploads/brands');
+        $img = Image::read($image->path());
+        $img->cover(124, 124, 'top');
+        $img->resize(124, 124, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($destination_path . '/' . $image_name);
+    }
+}
